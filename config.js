e847a353ab192178e18ff8c4edad7b7984b7ee76
WestleyCer <wacwestley@gmail.com>
2021-04-30 18:33:56 -0400

    fixed facebook bug

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
+const facebookSecret = require('./facebook');
+
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
     'facebook': {
         clientId: '1178224932693689',
-        clientSecret: '4e2a3faa7edbd41aa57b58a7bac631e6'
+        clientSecret: facebookSecret
     }
 }
---
+++ facebook.js
@@ -0,0 +1,3 @@
+const facebookSecret = '4e2a3faa7edbd41aa57b58a7bac631e6';
+
+module.exports = facebookSecret;
